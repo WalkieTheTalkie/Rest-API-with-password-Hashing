8bfa9c8761bfef8026084ded344be8f518ef44c3
Luke Gaudiano <lukegaudiano@yahoo.com>
2022-04-13 20:19:17 -0500

    first commit

---
+++ database/mongodb.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+
+mongoose.connect('mongodb+srv://BigBoy:Password@node-express-jwt-test.na0gp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+export default mongoose;
---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import parser from 'body-parser';
+import apiRoutes from './routes/api';
+
+const app = new express();
+app.use(parser.urlencoded({
+    extended: false
+}));
+app.use('/api',apiRoutes)
+
+app.get('/', (req, res) => {
+    return res.send('Hello World');
+});
+
+
+app.listen(process.env.PORT || 3000);
